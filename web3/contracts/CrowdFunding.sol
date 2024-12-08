34f99c84ed08a13ddfa6e58dd6b1dce9b1a3cb6e
cyrus_w <cyrusw89@gmail.com>
2024-12-09 04:20:05 +1300

    fix: CrowdFunding.sol

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -39,7 +39,7 @@ contract CrowdFunding {
     function donateToCampaign(uint256 _id) public payable {
         uint256 amount = msg.value;
 
-        Campaign storage campaign = 1[_id];
+        Campaign storage campaign = campaigns[_id];
 
         campaign.donators.push(msg.sender);
         campaign.donations.push(amount);
