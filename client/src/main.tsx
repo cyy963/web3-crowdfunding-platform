3d82e2c15f89315df75b3eeb1ce6acd7cc4df54d
cyrus_w <cyrusw89@gmail.com>
2024-12-09 05:15:44 +1300

    update: tailwind config, index.css, main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import App from "./App";
 import { ThirdwebProvider } from "thirdweb/react";
 import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThirdwebProvider>
-      <App />
-    </ThirdwebProvider>
+    <Router >
+      <ThirdwebProvider>
+        <App />
+      </ThirdwebProvider>
+    </Router>
   </React.StrictMode>
 );
