2412ab7d38683bcf7274071be8fac20e1614643b
cyrus_w <cyrusw89@gmail.com>
2025-01-07 16:02:36 +1300

    create: footer banner

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <div className="fixed bottom-0 w-[100vw] h-auto bg-purple-400 p-4 flex items-center justify-center">
+      <p className="font-normal text-white text-xl">
+        To test the features, connect your metamask wallet and switch to the
+        sepolia testnet and request free test tokens {' '}
+        <a
+          href="https://cloud.google.com/application/web3/faucet/ethereum/sepolia"
+          className="underline text-blue-300 hover:text-blue-400"
+        >
+        here
+        </a>
+        {'.'}
+      </p>
+    </div>
+  );
+};
+export default Footer;
