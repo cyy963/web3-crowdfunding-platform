dc652880df17585b348b9c0eb8ea76475efa890f
cyrus_w <cyrusw89@gmail.com>
2025-01-07 16:30:56 +1300

    update: mobile css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
     <main className="w-full">
       <div className="mx-auto max-w-7xl mt-4 px-4 md:px-12">
         <h1 className="text-4xl font-medium py-14">Fundraisers</h1>
-        <div className="grid grid-cols-3 gap-12 max-md:flex max-md:flex-col">
+        <div className="grid grid-cols-3 gap-12 max-md:flex max-md:flex-col pb-16">
           {!isLoadingCampaigns &&
             campaigns &&
             (campaigns.length > 0 ? (
